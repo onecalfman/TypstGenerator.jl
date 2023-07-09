988b396e62b1a32b0d63ee09c8836619142310a7
Jonas Walkling <jonas@walkling.email>
2023-07-09 14:25:14 +0200

    Remove too broad convert function

--- src/types.jl
+++ src/types.jl
@@ -280,9 +280,9 @@ function Base.convert(t :: Type{AbstractTypst}, str::String)::AbstractTypst
 	TypstBaseElement(TypstText, str, d)
 end
 
-function Base.convert(t :: Type{AbstractTypst}, str::Any)::AbstractTypst
+function Base.convert(t :: Type{AbstractTypst}, i::S) where { S <: Number }
 	d :: Dict{Symbol,Any} = Dict()
-	TypstBaseElement(TypstText, string(str), d)
+	TypstBaseElement(TypstText, string(i), d)
 end
 
 macro TypstTxtElem(t::Symbol)

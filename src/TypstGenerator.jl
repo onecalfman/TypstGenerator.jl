3b8f08fbee666206e219fade649793d3b7b3b846
Jonas Walkling <jonas@walkling.email>
2023-07-09 17:20:53 +0200

    Update example files

--- src/TypstGenerator.jl
+++ src/TypstGenerator.jl
@@ -2,11 +2,6 @@ module TypstGenerator
 
 using Measures
 
-try 
-    import Plots.grid 
-    import Plots.text
-catch end
-
 include("types.jl")
 include("render.jl")
 include("element_funtions.jl")
@@ -16,6 +11,6 @@ include("entrypoint.jl")
 export pt, mm, cm, align, block, bibliography, circle, cite, colbreak, document, ellipse, enum, figure,
 footnote, grid, h, heading, image, move, line, link, list, literal, lorem, numbering, outline, pad, page, pagebreak,
 par, path, place, polygon, math, rect, ref, rotate, scale, square, stack, table, terms, text, v, heading, luma,
-rgb, cmyk, angle, set, typst, deg, fr, +, *, /, -
+rgb, cmyk, angle, set, typst, deg, fr, +, *, /, -, columns
 
 end # module Typst
